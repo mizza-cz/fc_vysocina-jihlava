5b82ffa58f7be1bd36ba02bb01753e6a33f75ef9
mizza-cz <mizza.cz666@gmail.com>
2023-07-13 10:15:36 +0200

    add team-page

---
+++ src/components/team-section/team-section.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  var itemsPerPage = 3;
+  var visibleItems = itemsPerPage;
+
+  $(".season-list li").hide(); 
+
+  $(".season-list li:lt(" + visibleItems + ")").show(); 
+
+  $("#loadMoreBtn").click(function (e) {
+    e.preventDefault();
+    $(".season-list li:hidden:lt(" + itemsPerPage + ")").show();
+
+    if ($(".season-list li:hidden").length === 0) {
+      $("#loadMoreBtn").hide(); 
+    }
+  });
+});
